1a7f8722d97f16a16592be6c3e3a06e060ab2f05
fuzi <fzein.1695@gmail.com>
2025-10-23 08:47:34 +0700

    add: one to many - region to country

--- src/main/java/com/metrodata/serverapp/models/Country.java
+++ src/main/java/com/metrodata/serverapp/models/Country.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,4 +28,9 @@ public class Country {
 
   @Column(nullable = false, unique = true)
   private String countryName;
+
+  @ManyToOne
+  // @JsonBackReference
+  @JoinColumn(name = "region_id", nullable = false)
+  private Region region;
 }
--- src/main/java/com/metrodata/serverapp/models/Region.java
+++ src/main/java/com/metrodata/serverapp/models/Region.java
@@ -1,11 +1,15 @@
 package com.metrodata.serverapp.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +28,9 @@ public class Region {
 
   @Column(name = "region_name", nullable = false, unique = true, length = 20)
   private String nama;
+
+  @OneToMany(mappedBy = "region")
+  // @JsonManagedReference
+  @JsonProperty(access = Access.WRITE_ONLY)
+  private List<Country> countries;
 }
